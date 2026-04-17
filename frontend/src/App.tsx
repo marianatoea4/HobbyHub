7901d0087042e139222896ba68fe0074ece6f6e8
Toea Mariana <mariana.toea4@gmail.com>
2026-04-17 23:01:14 +0300

    Feat: Am adaugat Landing Page & favicon/logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,16 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
+import LandingPage from "./pages/LandingPage"; // <-- 1. Adaugă importul aici
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* redirectionam automat ruta principala (/) catre /login deocamdata */}
-        <Route path="/" element={<Navigate to="/login" />} />
+        {/* meow meow */}
+        <Route path="/" element={<LandingPage />} />
+
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
